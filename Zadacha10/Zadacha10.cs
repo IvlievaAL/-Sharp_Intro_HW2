e770aade09f7b90d3ec108a5cbd1fb4f68d20fe8
AlexIv <ivlieva.medphyslab.com>
2023-02-12 14:05:32 +0300

    Input of N added

--- Zadacha10/Zadacha10.cs
+++ Zadacha10/Zadacha10.cs
@@ -1,2 +1,5 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход
 //трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Enter number: ");
+string StrN = System.Console.ReadLine();
+int number = Convert.ToInt32(StrN);
