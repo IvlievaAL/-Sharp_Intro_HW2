11d51e3108d7295c25994e8b758b4e678c45e9f1
AlexIv <ivlieva.medphyslab.com>
2023-02-12 14:15:36 +0300

    Input of number added

--- Zadacha15/Zadacha15.cs
+++ Zadacha15/Zadacha15.cs
@@ -1,3 +1,6 @@
 ﻿//Задача 15: Напишите программу, которая принимает на вход
 //цифру, обозначающую день недели, и проверяет, является ли
 //этот день выходным.
+System.Console.WriteLine("Enter number of the day of the week: ");
+string StrD = System.Console.ReadLine();
+int Day = Convert.ToInt32(StrD);
