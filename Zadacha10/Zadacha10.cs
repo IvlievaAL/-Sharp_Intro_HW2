c3de60aadfc92c043a61c0ee5de493c42b09b1c7
AlexIv <ivlieva.medphyslab.com>
2023-02-12 14:10:06 +0300

    Code is written

--- Zadacha10/Zadacha10.cs
+++ Zadacha10/Zadacha10.cs
@@ -3,3 +3,13 @@
 System.Console.WriteLine("Enter number: ");
 string StrN = System.Console.ReadLine();
 int number = Convert.ToInt32(StrN);
+if (number<100|number>999)
+  {
+    System.Console.WriteLine("Число должно быть трехзначным");
+  }
+else 
+  {
+    int Ndigits=number/10;
+    int Nresult=Ndigits%10;
+    System.Console.WriteLine(Nresult);
+  }
