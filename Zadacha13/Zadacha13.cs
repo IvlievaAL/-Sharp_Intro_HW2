1e24ab43ad087f502eb68774bc00f1c6380ae6be
AlexIv <ivlieva.medphyslab.com>
2023-02-12 14:54:41 +0300

    How to count digits began

--- Zadacha13/Zadacha13.cs
+++ Zadacha13/Zadacha13.cs
@@ -3,3 +3,11 @@
 System.Console.WriteLine("Enter number: ");
 string StrN = System.Console.ReadLine();
 int number = Convert.ToInt32(StrN);
+int Ndigits;
+int count=0;
+while (Math.Abs(number)/10>0,1|number==0)
+   {
+    Ndigits++;
+    count++;
+   }
+System.Console.WriteLine("works");
