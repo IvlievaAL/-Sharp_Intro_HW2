59135b43ca8ec053a640782712375244c9a9d6a4
AlexIv <ivlieva.medphyslab.com>
2023-02-12 14:27:10 +0300

    Input of number added

--- Zadacha13/Zadacha13.cs
+++ Zadacha13/Zadacha13.cs
@@ -1,2 +1,5 @@
 ﻿//Напишите программу, которая выводит третью
 //цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Enter number: ");
+string StrN = System.Console.ReadLine();
+int number = Convert.ToInt32(StrN);
