6eab40d554f8a6f0070729e032bfd4c08059db88
AlexIv <ivlieva.medphyslab.com>
2023-02-12 19:00:27 +0300

    Massive completed, fills from right to left correctly

--- Zadacha13/Zadacha13.cs
+++ Zadacha13/Zadacha13.cs
@@ -13,13 +13,18 @@ while (Ndivided>=1)
    }
 int[] arrayN=new int[Ndigits];
 int index=Ndigits-1;
-int Ndivided1=(absnumber%10);
+Ndivided=Ndivided+(absnumber/10);
+System.Console.WriteLine(Ndivided);
+int Ndivided1=absnumber%10;
+System.Console.WriteLine(Ndivided1);
 while (index>=0)
    {
       arrayN[index]=Ndivided1;
-      Ndivided1=Ndivided1/10;
+      Ndivided1=Ndivided%10;
+      Ndivided=Ndivided/10;
       index=index-1;
-      System.Console.WriteLine("итерация");
+      System.Console.WriteLine(Ndivided1);
+      System.Console.WriteLine(Ndivided);
    }
    var str = string.Join(" , ", arrayN);
    Console.WriteLine(str);
